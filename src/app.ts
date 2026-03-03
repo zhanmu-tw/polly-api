92f06c78a7e86f04a3cc183c7e21928aeff97a35
James Duffield <james.duffield@luanta.com.tw>
2026-03-03 14:46:40 +0800

    fe

--- src/app.ts
+++ src/app.ts
@@ -28,7 +28,7 @@ app.get("/health", (_req, res) => {
 const frontendPath = path.join(__dirname, "../../frontend/dist");
 app.use(express.static(frontendPath));
 
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(frontendPath, "index.html"));
 });
 
